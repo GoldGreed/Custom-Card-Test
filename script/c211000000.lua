4747210fee0f996eed6f6c12b48cdd06b0801493
GoldGreed <omicciolisilvio@gmail.com>
2024-03-07 17:20:44 +0100

    Update c211000000.lua

--- script/c211000000.lua
+++ script/c211000000.lua
@@ -7,15 +7,9 @@ function s.initial_effect(c)
 		e1:SetValue(1)
 		c:RegisterEffect(e1)
 
-			--Apply effects at the end of the opponent's Battle Phase
 		local e2=Effect.CreateEffect(c)
-		e2:SetDescription(aux.Stringid(id,1))
-		e2:SetCategory(CATEGORY_DISABLE)
-		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-		e2:SetCode(EVENT_PHASE+PHASE_END)
-		e2:SetRange(LOCATION_MZONE)
-		e2:SetCondition(function(e,tp) return Duel.IsTurnPlayer(1-tp) end)
-		e2:SetTarget(s.efftg)
+		e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+		e2:SetCode(EVENT_SUMMON_SUCCESS)
 		e2:SetOperation(s.effop)
 		c:RegisterEffect(e2)
 	
