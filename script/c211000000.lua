5477dc9d37724d2112671eb47b1e82dc876c3cbd
GoldGreed <omicciolisilvio@gmail.com>
2024-03-07 17:25:13 +0100

    Update c211000000.lua

--- script/c211000000.lua
+++ script/c211000000.lua
@@ -10,13 +10,7 @@ function s.initial_effect(c)
 		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 		e2:SetCode(EVENT_SUMMON_SUCCESS)
-		e2:SetOperation(s.effop)e1:SetDescription(aux.Stringid(id,2))
-		e2:SetCategory(CATEGORY_TOHAND)
-		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-		e2:SetRange(LOCATION_MZONE)
-		e2:SetCode(EVENT_PHASE+PHASE_END)
-		e2:SetOperation(s.retop)
-		e2:SetReset(RESET_EVENT|RESETS_STANDARD&~(RESET_TEMP_REMOVE|RESET_TURN_SET)|RESET_PHASE|PHASE_END)
+		e2:SetOperation(s.effop)
 		c:RegisterEffect(e2)
 	
 end
@@ -26,6 +20,24 @@ function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE,nil,1,1-tp,LOCATION_MZONE)
 end
 
+function s.effop(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,2))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetTarget(s.rettg)
+	e1:SetOperation(s.retop)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD&~(RESET_TEMP_REMOVE|RESET_TURN_SET)|RESET_PHASE|PHASE_END)
+	c:RegisterEffect(e1)
+end
+
+function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
 
 function s.retop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
