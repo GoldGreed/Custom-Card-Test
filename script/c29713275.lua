c2574bc4fd1e88fad22a6bce379c9e5fc92a1f94
GoldGreed <omicciolisilvio@gmail.com>
2024-03-15 22:16:43 +0100

    Squadrion Trinatis added

---
+++ script/c29713275.lua
@@ -0,0 +1,28 @@
+--Squadrion Trinatis
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--cannot be target
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(aux.tgoval)
+	c:RegisterEffect(e1)
+	--gain attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetValue(s.atkval)
+	c:RegisterEffect(e2)
+	--pierce
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_PIERCE)
+	c:RegisterEffect(e3)
+end
+
+function s.atkval(e,c)
+	local LOCATION_GRAVE_MZONE=LOCATION_GRAVE+LOCATION_MZONE
+	return Duel.GetMatchingGroupCount(Card.IsSetCard,e:GetHandlerPlayer(),LOCATION_GRAVE,0,nil,0x7e5)*300
+end
