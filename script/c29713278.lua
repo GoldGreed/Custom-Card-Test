b61e1fe512c9552716055145e7a225c92dc48edf
GoldGreed <omicciolisilvio@gmail.com>
2024-03-16 12:21:44 +0100

    Added Dualis

---
+++ script/c29713278.lua
@@ -0,0 +1,7 @@
+--Squadrion Dualis
+local s,id,o=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Fusion Materials
+	Fusion.AddProcMix(c,true,true,(Card.IsSetCard,0x7e5),aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT))
+end
