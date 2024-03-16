6d897fd64aa56bbe915612e08b95a5179ddcc79c
GoldGreed <omicciolisilvio@gmail.com>
2024-03-16 12:25:12 +0100

    Update c29713278.lua

--- script/c29713278.lua
+++ script/c29713278.lua
@@ -3,5 +3,5 @@ local s,id,o=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--Fusion Materials
-	Fusion.AddProcMix(c,true,true,64631466,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT))
+	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsSetCard,0x7e5),aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT))
 end
