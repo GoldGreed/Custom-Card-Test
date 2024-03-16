687e09d02e21e760e511359c5af847c63e7ab465
GoldGreed <omicciolisilvio@gmail.com>
2024-03-16 11:03:31 +0100

    Sovereign added

---
+++ script/c29713277.lua
@@ -0,0 +1,32 @@
+--Alias Sovereign
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--cannot target
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e1:SetValue(aux.tgoval)
+	c:RegisterEffect(e1)
+	--indes
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(s.indval)
+	c:RegisterEffect(e2)
+	--atk limit
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(0,LOCATION_MZONE)
+	e3:SetValue(s.atlimit)
+	c:RegisterEffect(e3)
+end
+
+function s.atlimit(e,c)
+	return c~=e:GetHandler()
+end
