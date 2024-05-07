9ad35a2627994f5e75ccac54c6962c470ae56510
GoldGreed <omicciolisilvio@gmail.com>
2024-05-07 21:10:28 +0200

    Update c29713274.lua

--- script/c29713274.lua
+++ script/c29713274.lua
@@ -4,21 +4,32 @@ function s.initial_effect(c)
 	--Xyz Summon
 	Xyz.AddProcedure(c,nil,4,2)
 	c:EnableReviveLimit()
+	--pendulum summon
+	Pendulum.AddProcedure(c,false)
 	--destroy
+	--pendulum set
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DESTROY)
-	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1,29713274)
-	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
-	e1:SetCost(c29713274.descost)
-	e1:SetTarget(c29713274.destg)
-	e1:SetOperation(c29713274.desop)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.pctg)
+	e1:SetOperation(s.pcop)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,29713274)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e2:SetCost(c29713274.descost)
+	e2:SetTarget(c29713274.destg)
+	e2:SetOperation(c29713274.desop)
 	c:RegisterEffect(e2)
 end
-
+s.pendulum_level=7
 function c29713274.descost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(1)
 	return true
@@ -52,4 +63,19 @@ function c29713274.desop(e,tp,eg,ep,ev,re,r,rp)
 end
 function c29713274.costfilter(c,ec,tp)
 	return c:IsCode(29713274) and Duel.IsExistingTarget(c29713274.desfilter,tp,0,LOCATION_ONFIELD,1,c,c,ec)
+end
+
+function s.pctg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckPendulumZones(tp)
+		and Duel.IsExistingMatchingCard(s.pcfilter,tp,LOCATION_DECK,0,1,nil) end
+end
+
+function s.pcop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	if not Duel.CheckPendulumZones(tp) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local g=Duel.SelectMatchingCard(tp,s.pcfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	end
 end
