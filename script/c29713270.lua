2efa4136f1c15821133fd5ed04b966247ed72951
GoldGreed <omicciolisilvio@gmail.com>
2024-05-20 11:35:16 +0200

    blah blah effect

--- script/c29713270.lua
+++ script/c29713270.lua
@@ -9,7 +9,7 @@ function s.initial_effect(c)
 	--atk down
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetRange(LOCATION_PZONE)
+	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetTargetRange(0,LOCATION_MZONE)
 	e1:SetValue(-300)
