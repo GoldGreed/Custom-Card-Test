b109dc2c17e5fd57930852287c57c8dba0076442
GoldGreed <92056642+GoldGreed@users.noreply.github.com>
2024-06-17 11:06:21 +0200

    lot of changes to crystal

--- script/c29713265.lua
+++ script/c29713265.lua
@@ -1,7 +1,7 @@
 --Crystal Clear Prison
 local s,id,o=GetID()
 function c29713265.initial_effect(c)
-	--Negate an activation, destroy the card and all cards in the same column
+	--destroy the card and all cards in the same column
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY)
@@ -23,18 +23,14 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 end
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:IsHasType(EFFECT_TYPE_ACTIVATE) end
 	local c=e:GetHandler()
-	local exc=(e:IsHasType(EFFECT_TYPE_ACTIVATE) and c:IsRelateToEffect(e)) and c or nil
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg+rc:GetColumnGroup():RemoveCard(exc),1,0,0)
+	local g=c:GetColumnGroup()+c
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
 end
 
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local exc=(e:IsHasType(EFFECT_TYPE_ACTIVATE) and c:IsRelateToEffect(e)) and c or nil
-	local colg=rc:GetColumnGroup()
-	if exc then colg:RemoveCard(exc) end
-	if Duel.Destroy(eg,REASON_EFFECT)>0 and #colg>0 then
-		Duel.BreakEffect()
-		Duel.Destroy(colg,REASON_EFFECT)
-	end
+	local g=c:GetColumnGroup()+c
+	Duel.Destroy(g,REASON_EFFECT)
 end
